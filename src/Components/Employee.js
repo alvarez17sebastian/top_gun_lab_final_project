1ce355ad9105488ae68569ae43ceaadd8a5c5f78
jondas24 <jondas24@gmail.com>
2019-06-24 22:29:55 -0500

    Organice la estructura css de las tarjetas

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -6,17 +6,12 @@ export class Employee  extends Component{
     render(){
          const{photo,name,totalPoints}=this.props
         return (
-             <div class="card">
-                <div class="card-image">
-                    <figure >
-                     <img src={photo} />
-                    </figure>
-                </div>
-             <div class="card-content">
-                 <p class="title is-4" >{name}</p>
-                    <div >
-                     <a href="#">#totalPoints: {totalPoints}</a> 
-                    </div>
+        <div class="container">
+            <div class="card">
+              <img src={photo} />
+              <h1 >Trabajador: {name} </h1> 
+
+              <p>puntos Totales:  {totalPoints}</p>
             </div>
         </div>
        )
